011879a8f32e56e9400f70b59e3138b73cac5dc1
kimnari1073 <nari1073@naver.com>
2022-12-21 21:05:10 +0900

    12/21 web
    aside ing

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 $('.color-bt h3').click(function(){
     $(this).addClass('active')
     $(this).siblings().removeClass('active')
+    let img = $(this).parent().parent().siblings('#best');
+    if($(this).text()!=img.attr("src").match($(this).text())){
+        img.attr("src",function(index,attr){
+            if(attr.match('worst')) return attr.replace("worst","best")
+            else return attr.replace('best','worst')
+        })
+    }
 })
-function toggleImg(){
-    const best = document.getElementsById('best');
-    best.style.display = 'none';
-}
+// function asideScrol(name){
+//     let location = docuemnt.querySelector(name).offsetTop;
+//     window.scrollTo({top: location, behavior:'smooth'})
+// }
